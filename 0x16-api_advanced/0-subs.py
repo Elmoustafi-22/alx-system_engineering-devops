de81ebf636150e4abcc2e00a35563ec1cfe07617
root <mustophaqadir@gmail.com>
2024-03-07 11:41:17 -0800

    update

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+ #!/usr/bin/python3
+"""Query subscribers on a given Reddit subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        return 0
